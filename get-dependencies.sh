58a202bf3a9190156cb34241a773927ed82f0c65
Link4Electronics <link4electronics@gmail.com>
2026-01-24 20:24:50 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,13 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm
+pacman -Syu --noconfirm hatari
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package nestopia
+#make-aur-package 
 
 # If the application needs to be manually built that has to be done down here
