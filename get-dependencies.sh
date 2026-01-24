d0b7b134e1f75bb25362762f0d720499b790ee41
Link4Electronics <link4electronics@gmail.com>
2026-01-24 20:03:41 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,15 +6,13 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm fceux \
-    ffmpeg \
-    x265
+pacman -Syu --noconfirm
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package nestopia
 
 # If the application needs to be manually built that has to be done down here
