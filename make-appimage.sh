19a7964cb2bec9f3ba7fde260dd9f1029bd42f0e
Link4Electronics <link4electronics@gmail.com>
2026-01-25 07:11:48 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -14,6 +14,8 @@ export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/nestopia
+cp -r /usr/share/nestopia ./AppDir/share
+echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 
 # Additional changes can be done in between here
 
