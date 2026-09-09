ee7ba3b178c62df2ac047971faa719b6b9369076
Link4Electronics <link4electronics@gmail.com>
2026-09-08 22:47:32 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,8 +8,7 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://gitlab.com/jgemu/nestopia/-/raw/master/icons/nestopia256.png?ref_type=heads
-export DESKTOP=/usr/share/applications/nestopia.desktop
-export STARTUPWMCLASS=nestopia
+export DESKTOP=https://gitlab.com/jgemu/nestopia/-/raw/master/nestopia-jg.desktop
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
